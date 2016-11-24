64cd8d75689d5755d5187be61f8c3e191347432f
reedv <villanuevareed@gmail.com>
2016-11-23 15:56:21 -1000

    formatting

--- driver.c
+++ driver.c
@@ -17,8 +17,11 @@ MODULE_LICENSE("Dual BSD/GPL");
 /* Declaration of memory.c functions */
 int memory_open(struct inode *inode, struct file *filp);
 int memory_release(struct inode *inode, struct file *filp);
-ssize_t memory_read(struct file *filp, char *buf, size_t count, loff_t *f_pos); ssize_t memory_write(struct file *filp, char *buf, size_t count, loff_t *f_pos); void memory_exit(void);
+ssize_t memory_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+ssize_t memory_write(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+void memory_exit(void);
 int memory_init(void);
+
 /* Structure that declares the usual file */
 /* access functions */
 struct file_operations memory_fops = {
@@ -27,6 +30,7 @@ struct file_operations memory_fops = {
   open: memory_open,
   release: memory_release
 };
+
 /* Declaration of the init and exit functions */
 module_init(memory_init);
 module_exit(memory_exit);
