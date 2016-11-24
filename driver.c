88540d9a30bb910a36bb97104a7e8a3a627c2782
reedv <villanuevareed@gmail.com>
2016-11-23 15:56:31 -1000

    formatting
    removed old #includes
    
    
    removed old #includes
    
    
    fixed format errors

--- driver.c
+++ driver.c
@@ -1,21 +1,27 @@
 #include <linux/init.h>
-#include <linux/config.h>
+//#include <linux/config.h>  // see http://askubuntu.com/a/295142
+//#include <linux/autoconf.h>
 #include <linux/module.h>
-#include <linux/kernel.h> /* printk() */
-#include <linux/slab.h> /* kmalloc() */
-#include <linux/fs.h> /* everything... */
-#include <linux/errno.h> /* error codes */
-#include <linux/types.h> /* size_t */
+#include <linux/kernel.h> 	/* printk() */
+#include <linux/slab.h> 	/* kmalloc() */
+#include <linux/fs.h> 		/* everything... */
+#include <linux/errno.h> 	/* error codes */
+#include <linux/types.h> 	/* size_t */
 #include <linux/proc_fs.h>
-#include <linux/fcntl.h> /* O_ACCMODE */ #include <asm/system.h> /* cli(), *_flags */ #include <asm/uaccess.h> /* copy_from/to_user */
+#include <linux/fcntl.h> 	/* O_ACCMODE */
+//#include <asm/system.h> 	/* cli(), *_flags */  see http://superuser.com/a/783289
+#include <asm/uaccess.h> 	/* copy_from/to_user */
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 /* Declaration of memory.c functions */
 int memory_open(struct inode *inode, struct file *filp);
 int memory_release(struct inode *inode, struct file *filp);
-ssize_t memory_read(struct file *filp, char *buf, size_t count, loff_t *f_pos); ssize_t memory_write(struct file *filp, char *buf, size_t count, loff_t *f_pos); void memory_exit(void);
+ssize_t memory_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+ssize_t memory_write(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+void memory_exit(void);
 int memory_init(void);
+
 /* Structure that declares the usual file */
 /* access functions */
 struct file_operations memory_fops = {
@@ -24,6 +30,7 @@ struct file_operations memory_fops = {
   open: memory_open,
   release: memory_release
 };
+
 /* Declaration of the init and exit functions */
 module_init(memory_init);
 module_exit(memory_exit);
