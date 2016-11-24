f60706cafbae4e7e0758f95cb4e2f711e36c0c05
reedv <villanuevareed@gmail.com>
2016-11-23 15:42:26 -1000

    fixed format errors

--- driver.c
+++ driver.c
@@ -1,13 +1,15 @@
 #include <linux/init.h>
 #include <linux/config.h>
 #include <linux/module.h>
-#include <linux/kernel.h> /* printk() */
-#include <linux/slab.h> /* kmalloc() */
-#include <linux/fs.h> /* everything... */
-#include <linux/errno.h> /* error codes */
-#include <linux/types.h> /* size_t */
+#include <linux/kernel.h> 	/* printk() */
+#include <linux/slab.h> 	/* kmalloc() */
+#include <linux/fs.h> 		/* everything... */
+#include <linux/errno.h> 	/* error codes */
+#include <linux/types.h> 	/* size_t */
 #include <linux/proc_fs.h>
-#include <linux/fcntl.h> /* O_ACCMODE */ #include <asm/system.h> /* cli(), *_flags */ #include <asm/uaccess.h> /* copy_from/to_user */
+#include <linux/fcntl.h> 	/* O_ACCMODE */
+#include <asm/system.h> 	/* cli(), *_flags */
+#include <asm/uaccess.h> 	/* copy_from/to_user */
 
 MODULE_LICENSE("Dual BSD/GPL");
 
